9accd76cb8e4e4618230610801970307589bd843
eri-Q <eri.hatanaka8@gmail.com>
2023-04-11 17:34:15 +0900

    "done"

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import './service/firebase';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import { AuthProvider } from './providers/AuthProvider';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <AuthProvider>
       <Header />
-      
+      <Dashboard />
+      <Footer />
     </AuthProvider>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,45 @@ import React, { useContext } from "react";
 import dig from "object-dig"
 import { signInWithGoogle, logOut } from "../service/firebase";
 import { AuthContext } from "../providers/AuthProvider";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles (() => ({
+  toolbar: {
+    justifyContent: 'space-between',
+  },
+  button : {
+    color: '#fff'
+  }
+}))
 
 const Header = () => {
   const currentUser = useContext(AuthContext);
   console.log(currentUser)
   const button = () => {
     let buttonDom
-    if(dig(currentUser, 'currentUser') ) {
-      buttonDom = <button onClick={logOut} >ログアウト</button>
+    if(dig(currentUser, 'currentUser', 'uid') ) {
+      buttonDom = <Button className={classes.button} variant="inherit" onClick={logOut} >ログアウト</Button>
     }else{
-      buttonDom = <button onClick={signInWithGoogle} >ログイン</button>
+      buttonDom = <Button className={classes.button} variant="inherit" onClick={signInWithGoogle} >ログイン</Button>
     }
     return buttonDom
   }
 
+  const classes = useStyles();
+
   return(
-    <header>
-      ヘッダー
-      {button()}
-    </header>
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h6">
+          React Todo
+        </Typography>
+        {button()}
+      </Toolbar>
+    </AppBar>
   )
 }
 
